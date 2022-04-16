010fcc1340631bb61e60a331d9326c680ed5190d
Akhilesh Gavhane <akhipg@gmail.com>
2022-04-16 16:17:42 +0530

    Commit

--- chat.py
+++ chat.py
@@ -1,7 +1,73 @@
+# import random
+# import json
+
+# import torch
+
+# from model import NeuralNet
+# from nltk_utils import bag_of_words, tokenize
+
+# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# with open('intents.json', 'r') as json_data:
+#     intents = json.load(json_data)
+
+# FILE = "data.pth"
+# data = torch.load(FILE)
+
+# input_size = data["input_size"]
+# hidden_size = data["hidden_size"]
+# output_size = data["output_size"]
+# all_words = data['all_words']
+# tags = data['tags']
+# model_state = data["model_state"]
+
+# model = NeuralNet(input_size, hidden_size, output_size).to(device)
+# model.load_state_dict(model_state)
+# model.eval()
+
+# bot_name = "Sam"
+
+# def get_response(msg):
+#     sentence = tokenize(msg)
+#     X = bag_of_words(sentence, all_words)
+#     X = X.reshape(1, X.shape[0])
+#     X = torch.from_numpy(X).to(device)
+
+#     output = model(X)
+#     _, predicted = torch.max(output, dim=1)
+
+#     tag = tags[predicted.item()]
+
+#     probs = torch.softmax(output, dim=1)
+#     prob = probs[0][predicted.item()]
+#     if prob.item() > 0.75:
+#         for intent in intents['intents']:
+#             if tag == intent["tag"]:
+#                 return random.choice(intent['responses'])
+    
+#     return "I do not understand..."
+
+
+# if __name__ == "__main__":
+#     print("Let's chat! (type 'quit' to exit)")
+#     while True:
+#         # sentence = "do you use credit cards?"
+#         sentence = input("You: ")
+#         if sentence == "quit":
+#             break
+
+#         resp = get_response(sentence)
+#         print(resp)
+
+
+
 import random
 import json
 
 import torch
+
+import speech_recognition as sr
+import pyttsx3
 
 from model import NeuralNet
 from nltk_utils import bag_of_words, tokenize
@@ -25,7 +91,18 @@ model = NeuralNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
 
+# voice
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
 bot_name = "Sam"
+
+# voice
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
 
 def get_response(msg):
     sentence = tokenize(msg)
@@ -48,14 +125,32 @@ def get_response(msg):
     return "I do not understand..."
 
 
+def takeCommand():
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = r.listen(source)
+
+        try:
+            statement=r.recognize_google(audio,language='en-in')
+            print(f"user said:{statement}\n")
+
+        except Exception as e:
+            speak("Pardon me, please say that again")
+            return "None"
+        return statement
+
+
 if __name__ == "__main__":
     print("Let's chat! (type 'quit' to exit)")
     while True:
         # sentence = "do you use credit cards?"
-        sentence = input("You: ")
+        sentence = takeCommand().lower()
+    
         if sentence == "quit":
             break
 
         resp = get_response(sentence)
-        print(resp)
+        speak(resp)
 
